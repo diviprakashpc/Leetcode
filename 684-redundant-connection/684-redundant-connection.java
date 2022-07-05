6909d11346de854094e1a1c2e98cbf7144ca424a
Divya Prakash <86423544+diviprakashpc@users.noreply.github.com>
2022-07-05 23:28:18 +0530

    Time: 1 ms (87.43%), Space: 43.7 MB (67.55%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.java
+++ 684-redundant-connection/684-redundant-connection.java
@@ -1,33 +1,19 @@
 class Solution {
     public int[] findRedundantConnection(int[][] edges) {
-        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-         boolean[] visited = new boolean[edges.length+1];
-        for(int i = 0 ; i < edges.length+1 ; i++) graph.add(new ArrayList<>());
-        for(int i = 0 ; i < edges.length ; i++){
-            Arrays.fill(visited,false);
-             graph.get(edges[i][0]).add(edges[i][1]);
-            graph.get(edges[i][1]).add(edges[i][0]);
-            if(helper(graph,visited,-1,edges[i][0])){
-                int[] ans = new int[2];
-                ans[0] = edges[i][0];
-                    ans[1] = edges[i][1];
-                return ans;
-            }        }
-        return new int[2];
+        par = new int[edges.length+1];
+        for (int i = 0; i < par.length; i++)
+            par[i] = i;
+        for (int[] e : edges)
+            if (find(e[0]) == find(e[1])) return e;
+            else union(e[0],e[1]);
+        return edges[0];
     }
-    
-    public boolean helper(ArrayList<ArrayList<Integer>> graph, boolean[] visited, int parent, int src){
-        
-        visited[src] = true;
-        
-        for(Integer i : graph.get(src)){
-            if(!visited[i]){
-                helper(graph,visited,src,i);
-            }else if(i!=parent){
-                return true;
-            }
-        }
-        return false;
-        
+    private int[] par;
+    private int find(int x) {
+        if (x != par[x]) par[x] = find(par[x]);
+        return par[x];
+    }
+    private void union(int x, int y) {
+        par[find(y)] = find(x);
     }
 }
