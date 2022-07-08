ed5482f14d9835f3daeb634b49d6ca15ec842e1f
Divya Prakash <86423544+diviprakashpc@users.noreply.github.com>
2022-07-08 18:26:57 +0530

    Time: 101 ms (25.82%), Space: 55.4 MB (14.40%) - LeetHub

--- 313-super-ugly-number/313-super-ugly-number.java
+++ 313-super-ugly-number/313-super-ugly-number.java
@@ -1,24 +1,37 @@
 class Solution {
     public int nthSuperUglyNumber(int n, int[] primes) {
-       int[] ptrs = new  int[primes.length];
-        Arrays.fill(ptrs,1);
+       // int[] ptrs = new  int[primes.length];
+       //  Arrays.fill(ptrs,1);
+        PriorityQueue<Tuple> pq = new PriorityQueue<>();
+        for(int i = 0 ; i < primes.length ; i++){
+            pq.add(new Tuple(primes[i],1,1L*primes[i]));
+        }
         long[] dp = new long[n+1];
         dp[1] = 1;
-        
-        for(int i = 2 ; i <dp.length ; i++){
-            long min = Long.MAX_VALUE;
-            for(int j = 0 ; j < primes.length ; j++){
-                min = Math.min(min,primes[j]*dp[ptrs[j]]);
+        for(int i = 2 ; i <dp.length ;){
+            Tuple t = pq.remove();
+            Long min = t.value;
+            if(dp[i-1]!=t.value){
+                 dp[i] = min;
+                i++;
             }
-              dp[i] = min;
-            for(int j = 0 ; j < primes.length ; j++){
-                if(primes[j]*dp[ptrs[j]]==min){
-                    ptrs[j]++;
-                }
+           
+            pq.add(new Tuple(t.prime,t.ptr+1,t.prime*dp[t.ptr+1]));
             }
-          
-        }
-        
         return (int)dp[n];
+    }
+}
+
+class Tuple implements Comparable<Tuple>{
+    int prime;
+    int ptr;
+    long value;
+    Tuple(int prime,int ptr,long value){
+        this.prime = prime;
+        this.ptr = ptr;
+        this.value = value;
+    }
+    public int compareTo(Tuple t){
+        return (int)this.value - (int)t.value; 
     }
 }
