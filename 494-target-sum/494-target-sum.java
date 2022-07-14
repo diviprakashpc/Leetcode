7a7d981734262330b32f533cbbfd2853ba739283
Divya Prakash <86423544+diviprakashpc@users.noreply.github.com>
2022-07-14 20:31:48 +0530

    Time: 9 ms (79.99%), Space: 43.7 MB (42.29%) - LeetHub

--- 494-target-sum/494-target-sum.java
+++ 494-target-sum/494-target-sum.java
@@ -1,26 +1,28 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        int sum= Arrays.stream(nums).sum();
-        int[][] dp = new int[nums.length][2*sum+1];
-       
-        for(int i = 0 ;  i < dp.length ; i++){
-            Arrays.fill(dp[i],-1);
-        }
-        return helper(nums,nums.length-1,0,target,sum,dp);
+     
+        return countPartitions(nums.length,target,nums);
+    }
+   public int countPartitions(int n, int d, int[] arr) {
+        // Write your code here.
+        int sum = Arrays.stream(arr).sum();
+        if(sum-d<0||(sum-d)%2!=0) return 0;
+        return helper(arr,(sum-d)/2);
     }
     
-    public int helper(int[] nums,int idx ,int curr, int target,int sum,int[][] dp){
-         if(idx==-1){
-             int count = 0;
-             if(target==curr){
-               count++;
-             }
-             return count;
-         }
-        if(dp[idx][sum+curr]!=-1) return dp[idx][sum+curr];
-        int add = helper(nums,idx-1,curr+nums[idx],target,sum,dp);
-        int subtract =helper(nums,idx-1,curr-nums[idx],target,sum,dp);
-      dp[idx][sum+curr] = add + subtract;
-        return dp[idx][sum+curr];
+    public int helper(int[] arr , int tar){
+         int[][] dp = new int[arr.length][tar+1];
+            if(arr[0]==0) dp[0][0] = 2;   
+            else dp[0][0] = 1;    
+            if(arr[0]!=0&&arr[0]<=tar) dp[0][arr[0]] = 1;  
+            for(int i = 1 ;  i < arr.length ; i++){
+                for(int j = 0 ; j <= tar ; j++){
+                        int take = (j>=arr[i]) ? dp[i-1][j-arr[i]] : 0;
+                        int nottake = dp[i-1][j];
+                        dp[i][j] = (take + nottake);
+                }
+            }
+            return dp[arr.length-1][tar];
     }
+   
 }
