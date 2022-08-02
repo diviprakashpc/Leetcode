da6e8a5e004380ac1d01c561b4284870392034a9
Divya Prakash <86423544+diviprakashpc@users.noreply.github.com>
2022-08-02 23:51:45 +0530

    Time: 13 ms (12.40%), Space: 44.4 MB (9.37%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.java
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.java
@@ -1,18 +1,24 @@
 class Solution {
     public int longestValidParentheses(String s) {
-        if(s.length()==0||s.length()==1) return 0;
-        int[] dp = new int[s.length()+1];
-        dp[0] = 0;
-        
-        int ans = 0;
-        for(int i = 1 ; i < dp.length ; i++){
-            char ch = s.charAt(i-1);
-            int j = i-1 - dp[i-1] -1;
-            if(j>=0&&s.charAt(j)=='('&&ch==')'){
-                dp[i]  = dp[i-1] + dp[j] + 2; 
-                ans = Math.max(ans,dp[i]);
+        Stack<Character> stack = new Stack<>();
+        Stack<Integer> index = new Stack<>();
+        index.push(-1);
+        int max = 0;
+        for(int i = 0 ; i < s.length() ; i++){
+            char ch = s.charAt(i);
+            if(ch=='('){
+                stack.push(ch);
+                index.push(i);
+            }else{
+                if(stack.isEmpty()){
+                    index.push(i);
+                }else{
+                    stack.pop();
+                    index.pop();
+                    max = Math.max(max,i-index.peek());
+                }
             }
         }
-        return ans;
+        return max;
     }
 }
