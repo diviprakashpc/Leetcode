bbdc135c2212f3541c84e4bf72cfdb279223e9da
Divya Prakash <86423544+diviprakashpc@users.noreply.github.com>
2022-08-27 19:49:48 +0530

    Time: 11 ms (42.96%), Space: 43.4 MB (45.60%) - LeetHub

--- 877-stone-game/877-stone-game.java
+++ 877-stone-game/877-stone-game.java
@@ -1,35 +1,22 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-        int n = piles.length;
-        boolean[][] dp = new boolean[n][n];
-        for(int i = n-1 ; i>=0 ; i--){
-            for(int j =  0 ; j <= i ; j++){
-                if(i>j) dp[i][j] = false;
-                else if(i==j) dp[i][j] = true;
-                else if(j==i+1) dp[i][j] = true;
+        //Use Gap strategy b/c feel aaati hai
+        boolean[][] dp = new boolean[piles.length][piles.length];
+        
+        for(int gap = 0 ; gap < dp.length ; gap++){
+            for(int i = 0,j = gap ; j < dp.length ; i++,j++){
+                if(gap==0){
+                    dp[i][j] = true;
+                }else if(gap==1){
+                    dp[i][j] = true;
+                }
                 else{
-                boolean left = (dp[i+2][j])&(dp[i+1][j-1]);
-                boolean right = (dp[i+1][j-1])&(dp[i+2][j-1]);
-                dp[i][j]= left|right;  
+                    boolean left = ((dp[i+2][j])&&(dp[i+1][j-1]));
+                    boolean right = ((dp[i+1][j-1])&&(dp[i][j-2]));
+                    dp[i][j] = left||right;
                 }
             }
         }
-        return dp[n-1][n-1];
-        
-        // return  helper(piles,0,piles.length-1); 
-    }
-    
-    public boolean helper(int[] arr , int i , int j){
-        if(i>j) return true;
-        //base case
-        if(i==j){
-           return true; 
-        }
-          if(j-i==1){
-              return true;
-          }
-        boolean left = (helper(arr,i+2,j)&helper(arr,i+1,j-1));
-        boolean right = (helper(arr,i+1,j-1)&helper(arr,i+2,j-1));
-        return left|right;
+        return dp[0][dp.length-1];
     }
 }
