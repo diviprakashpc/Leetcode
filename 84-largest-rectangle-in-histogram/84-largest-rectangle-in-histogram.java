fedef4c93313b6f49993e4f3e7d6d5b4a93f2014
Divya Prakash <diviprakashpc@gmail.com>
2022-09-07 19:14:56 +0530

    Time: 349 ms (14.81%), Space: 98.2 MB (22.00%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
@@ -2,29 +2,11 @@ class Solution {
     public int largestRectangleArea(int[] heights) {
         int[] nse = nextSmaller(heights);
         int[] pse = prevSmaller(heights);
-        // for(int i = 0 ; i < nse.length ; i++){
-        //     System.out.print(nse[i]+" ");
-        // }
-        // System.out.println();
-        // for(int i = 0 ; i < pse.length ; i++){
-        //     System.out.print(pse[i]+" ");
-        // }
-        // System.out.println();
         int maxArea = 0;
         for(int i = 0 ; i < heights.length ; i++){
             int li = (pse[i]==-1)?-1:pse[i];
             int ri = nse[i]==-1?heights.length:nse[i];
-            // if(li==-1&&ri==heights.length+1) maxArea = Math.max(maxArea,heights.length*heights[i]);
-            // else if(li==-1||ri==heights.length+1){
-            //    if(li==-1){
-            //        maxArea = Math.max(maxArea,ri*heights[i]);
-            //    }else{
-            //        maxArea = Math.max(maxArea,(heights.length-li-1)*heights[i]);
-            //    }
-            // }
-            // else
             maxArea = Math.max(maxArea,(ri-li-1)*heights[i]);
-         //   System.out.print(maxArea+" ");
         }
         return maxArea;
     }
