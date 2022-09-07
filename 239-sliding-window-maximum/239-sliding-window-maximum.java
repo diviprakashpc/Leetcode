691e9782e80a24716789802d798b08f21fc8f1f0
Divya Prakash <diviprakashpc@gmail.com>
2022-09-07 20:42:12 +0530

    Time: 2059 ms (5.01%), Space: 153.6 MB (32.47%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.java
+++ 239-sliding-window-maximum/239-sliding-window-maximum.java
@@ -1,29 +1,33 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        int i = 0;
-        int j = 0;
-        List<Integer> temp = new ArrayList<>();
-        int[] res = new int[nums.length-(k-1)];
+       int[] res = new int[nums.length-k+1];
+        int[] nge = nextGreaterElement(nums);
         int itr = 0;
-        while(j<nums.length){
-            if(temp.size()==0) temp.add(nums[j]);
-            else{
-                while(temp.size()!=0&&temp.get(temp.size()-1)<nums[j]){
-                    temp.remove(temp.size()-1);
-                }
-                temp.add(nums[j]);
-            }
-             if(j-i+1<k){
-                 j++;
-             }else{
-                 res[itr++] = temp.get(0);
-                 if(nums[i]==temp.get(0)){
-                     temp.remove(0);
-                 }
-                 i++;
-                 j++;
-             }
-        }
+       for(int i = 0 ; i <= nums.length-k ; i++){
+           int j = i;
+           while(nge[j]<i+k){
+               j = nge[j];
+           }
+           res[itr++] = nums[j];
+       }
         return res;
+    }
+    
+    public int[]  nextGreaterElement(int[] nums){
+        int[] nge = new int[nums.length];
+        Stack<Integer> st = new Stack<>();
+        // pop ans push
+        for(int i = nums.length - 1 ; i>=0 ; i--){
+            while(st.size()>0&&nums[i]>=nums[st.peek()]){
+                st.pop();
+            }
+            if(st.isEmpty()){
+                nge[i] = nums.length;
+            }else{
+                nge[i] = st.peek();
+            }
+            st.push(i);
+        }
+        return nge;
     }
 }
