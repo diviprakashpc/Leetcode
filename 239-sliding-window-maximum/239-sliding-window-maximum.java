36d55d1711f4a083158a8d944b659bcb005faa8d
Divya Prakash <diviprakashpc@gmail.com>
2022-09-07 20:25:37 +0530

    Time: 448 ms (6.32%), Space: 151.3 MB (40.43%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.java
+++ 239-sliding-window-maximum/239-sliding-window-maximum.java
@@ -1,32 +1,29 @@
 class Solution {
-    public int[] maxSlidingWindow(int[] arr, int k) {
+    public int[] maxSlidingWindow(int[] nums, int k) {
         int i = 0;
         int j = 0;
-        int[] res = new int[arr.length-(k-1)];
         List<Integer> temp = new ArrayList<>();
-        int kk = 0;
-        int currmax = Integer.MIN_VALUE;
-        while(j<arr.length){
-             if(temp.size()==0){
-                 temp.add(arr[j]);
-             }else{
-                 while(temp.size()!=0&&temp.get(temp.size()-1)<arr[j]){
-                     temp.remove(temp.size()-1);
-                 }
-                 temp.add(arr[j]);
-             }
-            if(j-i+1<k) j++;
-            else if(j-i+1==k){
-               res[kk++] = temp.get(0);
-                if(arr[i]==temp.get(0)){
-                   temp.remove(0);
+        int[] res = new int[nums.length-(k-1)];
+        int itr = 0;
+        while(j<nums.length){
+            if(temp.size()==0) temp.add(nums[j]);
+            else{
+                while(temp.size()!=0&&temp.get(temp.size()-1)<nums[j]){
+                    temp.remove(temp.size()-1);
                 }
-                i++;
-                j++;
+                temp.add(nums[j]);
             }
+             if(j-i+1<k){
+                 j++;
+             }else{
+                 res[itr++] = temp.get(0);
+                 if(nums[i]==temp.get(0)){
+                     temp.remove(0);
+                 }
+                 i++;
+                 j++;
+             }
         }
-        
-        
-       return res;
+        return res;
     }
 }
