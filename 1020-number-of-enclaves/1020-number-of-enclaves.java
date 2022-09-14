248f846bfac982b7e47a06eefff0d53f4ab80791
Divya Prakash <diviprakashpc@gmail.com>
2022-09-14 16:15:54 +0530

    Time: 30 ms (5.89%), Space: 76.5 MB (6.80%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,18 +1,10 @@
 class Solution {
     public int numEnclaves(int[][] grid) {
-      boolean[][] visited = new boolean[grid.length][grid[0].length];
-        // for(int i = 0 ; i < grid.length ; i++){
-        //     for(int j = 0 ; j < grid[0].length ; j++){
-        //         System.out.print(grid[i][j]+" ");
-        //     }
-        //     System.out.println();
-        // }
-        int noOfClosedIslands = 0;
         for(int i = 0 ; i < grid.length ; i++){
             for(int j = 0 ; j < grid[0].length ; j++){
-                if(!visited[i][j]&&grid[i][j]==1){
+                if(grid[i][j]==1){
                     if(i==0||i==grid.length-1||j==0||j==grid[0].length-1){
-                          dfs(i,j,grid,visited);
+                          dfs(i,j,grid);
                     }
                 }
             }
@@ -20,27 +12,23 @@ class Solution {
         int count = 0;
          for(int i = 0 ; i < grid.length ; i++){
             for(int j = 0 ; j < grid[0].length ; j++){
-                if(!visited[i][j]&&grid[i][j]==1){
+                if(grid[i][j]==1){
                     count++;
                 }
             }
         }
         return count;
-  
     }
-    public boolean isValid(int sr , int sc , int[][] grid,boolean[][] visited){
-        return !(sr<0||sr>=grid.length||sc<0||sc>=grid[0].length||visited[sr][sc]);
+    public boolean isValid(int sr , int sc , int[][] grid){
+        return !(sr<0||sr>=grid.length||sc<0||sc>=grid[0].length||grid[sr][sc]==0);
     }
     
-    public void dfs(int sr , int sc , int[][] grid , boolean[][] visited){
-       
-        visited[sr][sc] = true;
+    public void dfs(int sr , int sc , int[][] grid){
+        grid[sr][sc] = 0;
         int[][] dirs = {{0,1},{1,0},{0,-1},{-1,0}};
-        
         for(int i = 0 ; i < dirs.length ; i++){
-        if(isValid(sr+dirs[i][0],sc+dirs[i][1],grid,visited)&&grid[sr+dirs[i][0]][sc+dirs[i][1]]==1) {
-         
-            dfs(sr+dirs[i][0],sc+dirs[i][1],grid,visited);
+        if(isValid(sr+dirs[i][0],sc+dirs[i][1],grid)) {
+            dfs(sr+dirs[i][0],sc+dirs[i][1],grid);
         }
         }
     }
