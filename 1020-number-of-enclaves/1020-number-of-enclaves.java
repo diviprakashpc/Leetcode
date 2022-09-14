37b8f22e16662e06cfb643b9d8e2efac9f072afb
Divya Prakash <diviprakashpc@gmail.com>
2022-09-14 16:13:48 +0530

    Time: 17 ms (19.74%), Space: 57.9 MB (86.05%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -12,37 +12,35 @@ class Solution {
             for(int j = 0 ; j < grid[0].length ; j++){
                 if(!visited[i][j]&&grid[i][j]==1){
                     if(i==0||i==grid.length-1||j==0||j==grid[0].length-1){
-                          dfs(i,j,grid,visited,new int[1]);
+                          dfs(i,j,grid,visited);
                     }
                 }
             }
         }
+        int count = 0;
          for(int i = 0 ; i < grid.length ; i++){
             for(int j = 0 ; j < grid[0].length ; j++){
                 if(!visited[i][j]&&grid[i][j]==1){
-                    int[] count = new int[1];
-                    count[0] = 1;
-                    dfs(i,j,grid,visited,count);
-                    noOfClosedIslands+=count[0];
+                    count++;
                 }
             }
         }
-        return noOfClosedIslands;
+        return count;
   
     }
     public boolean isValid(int sr , int sc , int[][] grid,boolean[][] visited){
         return !(sr<0||sr>=grid.length||sc<0||sc>=grid[0].length||visited[sr][sc]);
     }
     
-    public void dfs(int sr , int sc , int[][] grid , boolean[][] visited,int[] count){
+    public void dfs(int sr , int sc , int[][] grid , boolean[][] visited){
        
         visited[sr][sc] = true;
         int[][] dirs = {{0,1},{1,0},{0,-1},{-1,0}};
         
         for(int i = 0 ; i < dirs.length ; i++){
         if(isValid(sr+dirs[i][0],sc+dirs[i][1],grid,visited)&&grid[sr+dirs[i][0]][sc+dirs[i][1]]==1) {
-            count[0]++;
-            dfs(sr+dirs[i][0],sc+dirs[i][1],grid,visited,count);
+         
+            dfs(sr+dirs[i][0],sc+dirs[i][1],grid,visited);
         }
         }
     }
