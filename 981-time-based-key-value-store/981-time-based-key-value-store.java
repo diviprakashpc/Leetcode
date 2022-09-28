9456dc2df6d6484a98316df650bc03a511262d28
Divya Prakash <diviprakashpc@gmail.com>
2022-09-28 09:42:11 +0530

    Time: 166 ms (92.67%), Space: 115.2 MB (99.49%) - LeetHub

--- 981-time-based-key-value-store/981-time-based-key-value-store.java
+++ 981-time-based-key-value-store/981-time-based-key-value-store.java
@@ -1,31 +1,54 @@
 class TimeMap {
-   
-    HashMap<String,TreeMap<Integer,String>> map;
+     
+    HashMap<String,List<Pair>> map;
     public TimeMap() {
         map = new HashMap<>();
     }
     
     public void set(String key, String value, int timestamp) {
-         if(!map.containsKey(key)){
-           map.put(key,new TreeMap<>());
-         }
-         map.get(key).put(timestamp,value);
+        if(!map.containsKey(key)){
+          map.put(key,new ArrayList<>());
+        }
+       Pair p = new Pair(value,timestamp);
+       map.get(key).add(p);
     }
     
     public String get(String key, int timestamp) {
-        
-      if(!map.containsKey(key)) return "";
-      
-      TreeMap<Integer,String> ts = map.get(key);
-      if(ts.containsKey(timestamp)) return ts.get(timestamp);
-      else{
-          Integer floor = ts.floorKey(timestamp);
-        if(floor!=null) return ts.get(floor);
-        return "";
-        }
+         if(!map.containsKey(key)) return "";
+        Pair lbp = lower_bound(map.get(key),timestamp);
+       if(lbp!=null)
+        return lbp.value;
+      return "";
+    }
+  
+  
+    public Pair lower_bound(List<Pair> list,int target){
+       int start = 0;
+       int end  = list.size()-1;
+       int ans = -1;
+       while(start<=end){
+         int mid = start + (end-start)/2;
+         if(list.get(mid).ts==target){
+           return list.get(mid);
+         }else if(list.get(mid).ts<target){
+               ans = Math.max(ans,mid);
+               start = mid + 1;
+         }else{
+              end = mid - 1;
+         }
+       }
+       return (ans==-1) ? null : list.get(ans);
+    }
+  
+    static class Pair{
+      String value;
+      int ts;
+      Pair(String value , int ts){
+        this.value = value;
+        this.ts = ts;
       }
     }
-
+}
 
 /**
  * Your TimeMap object will be instantiated and called as such:
