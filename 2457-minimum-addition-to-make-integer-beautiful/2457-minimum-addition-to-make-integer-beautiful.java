e1d783e74fe3e77b8a1d3a1d4dc5de692e200bd2
Divya Prakash <diviprakashpc@gmail.com>
2022-11-07 16:40:03 +0530

    Time: 1 ms (74.74%), Space: 40.1 MB (82.50%) - LeetHub

--- 2457-minimum-addition-to-make-integer-beautiful/2457-minimum-addition-to-make-integer-beautiful.java
+++ 2457-minimum-addition-to-make-integer-beautiful/2457-minimum-addition-to-make-integer-beautiful.java
@@ -1,37 +1,24 @@
 class Solution {
     public long makeIntegerBeautiful(long n, int target) {
-      
-      String num = Long.toString(n);
-      String ans = "";
-      int currsum = 0;
-      for(int i = 0 ; i < num.length() ; i++){
-          currsum+=(num.charAt(i)-'0');
+        if(sumOfDigits(n)<=target) return 0;
+        int power = 1;
+        long orignal_n = n;
+        while(true){
+          long n_state = ((n/(long)Math.pow(10,power))+1)*(long)Math.pow(10,power);
+          if(n_state==0) break;
+          if(sumOfDigits(n_state)<=target) return Math.abs(n - n_state);
+          power++;
         }
-      if(currsum<=target) return 0;
-      int j = num.length()-1;
-      while(j>=0&&num.charAt(j)=='0') {
-        ans = ans + '0';
-        j--;
+      return 0;
+    }
+  
+    public long sumOfDigits(long n){
+      long sum = 0;
+      
+      while(n!=0){
+        sum+=(n%10);
+        n = n/10;
       }
-      int val = num.charAt(j) -'0';
-      int toadd = 10 - val;
-      ans = Integer.toString(toadd)+ans;
-      currsum-=val;
-      currsum+=1;
-      int pc = 1;
-      if(currsum<=target) return Long.parseLong(ans);
-      for(int i = j-1 ; i>=0 ; i--){
-           if(currsum<=target){
-             return Long.parseLong(ans);
-          }
-           val = num.charAt(i) - '0' + pc;
-           toadd = 10 - val;
-          ans = Integer.toString(toadd) + ans;
-          currsum-=val;
-          currsum+=1;
-         
-      }
-       if(currsum<=target)  return Long.parseLong(ans);
-       return 1L;
+      return sum;
     }
 }
