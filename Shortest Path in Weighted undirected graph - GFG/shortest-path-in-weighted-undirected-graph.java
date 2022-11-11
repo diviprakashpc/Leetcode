19e8eb05cca4796a67f08cb3978bb4f2631f4525
Divya Prakash <diviprakashpc@gmail.com>
2022-11-11 13:27:40 +0530

    Added solution - LeetHub

--- Shortest Path in Weighted undirected graph - GFG/shortest-path-in-weighted-undirected-graph.java
+++ Shortest Path in Weighted undirected graph - GFG/shortest-path-in-weighted-undirected-graph.java
@@ -31,69 +31,75 @@ import java.io.*;
 
 // User function Template for Java
 
-class Solution {
-    static String shortestPath;
-    static class Node{
-        int value;
-        int wt;
-        Node(int val , int w){
-            value = val;
-            wt = w;
-        }
-    }
-    
-    static class Pair implements Comparable<Pair>{
-        int dist;
-        int nodeValue;
-        String sp;
-        Pair(int dist , int nodeValue, String sp){
-            this.dist = dist;
-            this.nodeValue = nodeValue;
-            this.sp = sp;
-        }
-        
-        public int compareTo(Pair p) {return this.dist - p.dist;}
-    }
-    public static List<Integer> shortestPath(int n, int m, int edges[][]) {
-        // code here
-        List<List<Node>> graph = new ArrayList<>();
-        for(int i =0  ; i <=n ; i++) graph.add(new ArrayList<>());
-        for(int[] row : edges){
-            int u = row[0];
-            int v = row[1];
-            int wt = row[2];
-            graph.get(u).add(new Node(v,wt));
-            graph.get(v).add(new Node(u,wt));
-        }
-        shortestPath ="";
-        int[] dist = new int[n+1];
-        Arrays.fill(dist,Integer.MAX_VALUE);
-        dist[0] = -1;
-        dist[1] = 0;
-        PriorityQueue<Pair> pq = new PriorityQueue<>();
-        pq.add(new Pair(0,1,"1$"));
-        while(!pq.isEmpty()){
-            Pair p = pq.remove();
-            int distance = p.dist;
-            int nodeValue = p.nodeValue;
-            String sp = p.sp;
-            for(Node nbr : graph.get(nodeValue)){
-                if(dist[nbr.value]>distance + nbr.wt){
-                    dist[nbr.value] = distance +nbr.wt;
-                    if(nbr.value==n){
-                        shortestPath = sp + Integer.toString(nbr.value)+'$';
-                        
-                    }
-                    pq.add(new Pair(dist[nbr.value],nbr.value,sp+nbr.value+'$'));
-                }
-            }
-        }
-        
-        List<Integer> ans = new ArrayList<>();
-      //  System.out.println(shortestPath);
-        String[] arr = shortestPath.split("\\$");
-        for(int i = 0 ; i < arr.length ; i++) if(arr[i].length()!=0) ans.add(Integer.parseInt(arr[i]));
-        if(ans.size()==0) ans.add(-1);
-        return ans;
-    }
+public class Solution {
+	
+	    static class Node{
+	        int value;
+	        int wt;
+	        Node(int val , int w){
+	            value = val;
+	            wt = w;
+	        }
+	    }
+	    
+	    static class Pair implements Comparable<Pair>{
+	        int dist;
+	        int nodeValue;
+	        Pair(int dist , int nodeValue){
+	            this.dist = dist;
+	            this.nodeValue = nodeValue;
+	            
+	        }
+	        
+	        public int compareTo(Pair p) {return this.dist - p.dist;}
+	    }
+	    public static List<Integer> shortestPath(int n, int m, int edges[][]) {
+	        // code here
+	        List<List<Node>> graph = new ArrayList<>();
+	        for(int i =0  ; i <=n ; i++) graph.add(new ArrayList<>());
+	        for(int[] row : edges){
+	            int u = row[0];
+	            int v = row[1];
+	            int wt = row[2];
+	            graph.get(u).add(new Node(v,wt));
+	            graph.get(v).add(new Node(u,wt));
+	        }
+	       
+	        int[] dist = new int[n+1];
+	        int[] parent = new int[n+1];
+	        Arrays.fill(dist,Integer.MAX_VALUE);
+	        for(int i = 1 ; i<=n ; i++) parent[i] = i;
+	        dist[0] = -1;
+	        dist[1] = 0;
+	        PriorityQueue<Pair> pq = new PriorityQueue<>();
+	        pq.add(new Pair(0,1));
+	        while(!pq.isEmpty()){
+	            Pair p = pq.remove();
+	            int distance = p.dist;
+	            int nodeValue = p.nodeValue;
+	           
+	            for(Node nbr : graph.get(nodeValue)){
+	                if(dist[nbr.value]>distance + nbr.wt){
+	                    dist[nbr.value] = distance +nbr.wt;
+	                    parent[nbr.value] = nodeValue;
+	                    pq.add(new Pair(dist[nbr.value],nbr.value));
+	                }
+	            }
+	        }
+	        
+	        
+	        int nodeValue = n;
+	        List<Integer> ans = new ArrayList<>();
+	        if(dist[nodeValue]==Integer.MAX_VALUE){
+	            ans.add(-1);
+	            return ans;
+	        }
+	        while(parent[nodeValue]!=nodeValue) {
+	        	ans.add(0, nodeValue);
+	        	nodeValue = parent[nodeValue];
+	        }
+	        ans.add(0,1);
+	        return ans;
+	    }
+
 }
