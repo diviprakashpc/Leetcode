b6ef1653bc0fb0137a3c53485c56fbba5c1640c8
Divya Prakash <diviprakashpc@gmail.com>
2022-11-13 20:22:03 +0530

    Time: 2 ms (88.53%), Space: 42.9 MB (93.41%) - LeetHub

--- 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.java
+++ 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.java
@@ -1,26 +1,34 @@
 class Solution {
     public int findUnsortedSubarray(int[] nums) {
-       Stack<Integer> st = new Stack<>();
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        boolean flag = false;
+       for(int i = 1; i < nums.length ; i++){
+         if(nums[i]<nums[i-1]){
+           flag = true;
+         }
+          if(flag) min = Math.min(min,nums[i]);
+       }
+        flag = false;
+       for(int i = nums.length-2; i>=0 ; i--){
+         if(nums[i]>nums[i+1]) flag = true;
+         if(flag) max = Math.max(max,nums[i]);
+       }
        int left = nums.length;
        int right = 0;
-       for(int i = 0 ;i < nums.length ; i++){
-          while(!st.isEmpty()&&nums[st.peek()]>nums[i]) {
-              left = Math.min(left,st.peek());
-              st.pop();
-          }
-         st.push(i);
+       for(int i = 0 ; i < nums.length ; i++){
+         if(nums[i]>min) {
+           left = i;
+           break;
+         }
        }
-    
-     st.clear();
-    
-     for(int i = nums.length-1 ; i>=0 ;i--){
-       while(!st.isEmpty()&&nums[st.peek()]<nums[i]){
-          right = Math.max(right,st.peek());
-          st.pop();
+      
+       for(int i = nums.length-1; i >=0 ;i--){
+         if(nums[i]<max) {
+           right = i;
+           break;
+         }
        }
-          st.push(i);
-     }
-    // System.out.println(left+" "+right);
-    return left<right?right-left+1:0;
-      }
+      return left<right ? right-left+1 : 0;
+    }
 }
