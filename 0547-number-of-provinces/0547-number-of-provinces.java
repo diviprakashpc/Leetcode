ba8024088e86aee7805a53f31907728073a55e7d
Divya Prakash <diviprakashpc@gmail.com>
2022-11-16 15:00:55 +0530

    Time: 4 ms (49.25%), Space: 53.9 MB (61.83%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,64 +1,24 @@
 class Solution {
-    public int findCircleNum(int[][] c) {
-       DisjointSet ds = new DisjointSet(c.length);
-       ds.makeSet(c.length);
-       for(int i =0 ; i < c.length ; i++){
-         for(int j = 0 ; j < c[0].length ; j++){
-              if(c[i][j]==1){
-                 ds.union_by_size(i,j);
-              }
+    public int findCircleNum(int[][] edges) {
+        boolean[] visited = new boolean[edges.length];
+        int no_p = 0;
+       for(int i = 0 ; i < edges.length ; i++){
+         for(int j = 0 ; j < edges[0].length ; j++){
+           if(!visited[j]&&edges[i][j]==1){
+             dfs(edges,i,visited);
+             no_p++;
+           }
          }
        }
-      
-      // for(int i = 0;  i < ds.parent.length ; i++){
-      //   System.out.print(ds.parent[i]+" ");
-      // }
-      int n_o_p = 0;
-     Set<Integer> set = new HashSet<>();
-       for(int i = 0;  i < c.length ; i++){
-          int leader = ds.find(i);
-          set.add(leader);
-       }
-      return set.size();
-      
+      return no_p;
     }
-    
-   static class DisjointSet{
-      int[] parent;
-      int[] size;
-     DisjointSet(int n){
-       parent = new int[n+1];
-       size = new int[n+1];
-     }
-     
-      void makeSet(int n){
-       for(int i = 0 ; i<=n ; i++){
-         parent[i] = i;
-         size[i] = 1;
-       }
-     }
-     
-      int find(int x){
-        if(x==parent[x]){
-           return x;
-        }
-        
-        int found = find(parent[x]);
-        parent[x] = found;
-        return found;
-     }
-     
-      void union_by_size(int x, int y){
-       int lx = find(x);
-       int ly = find(y);
-       if(lx==ly) return;
-       if(size[lx]>size[ly]){
-         parent[ly] = lx;
-         size[ly]+=size[lx];
-       }else{
-         parent[lx] = ly;
-         size[lx]+=size[ly];
-       }
+  
+   public void dfs(int[][] edges , int src , boolean[] visited){
+      visited[src] = true;
+     for(int j = 0; j < edges[src].length ; j++){
+         if(!visited[j]&&edges[src][j]==1){
+           dfs(edges,j,visited);
+         }
      }
    }
 }
