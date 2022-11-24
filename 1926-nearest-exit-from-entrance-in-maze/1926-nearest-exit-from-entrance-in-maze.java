7cedcfde9f3ba729d8402ffd12101a2952a7df74
Divya Prakash <diviprakashpc@gmail.com>
2022-11-24 10:01:03 +0530

    Time: 9 ms (80.35%), Space: 71.7 MB (50.66%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.java
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.java
@@ -6,6 +6,7 @@ class Solution {
         boolean[][] vis = new boolean[maze.length][maze[0].length];
         int er = entrance[0];
         int ec = entrance[1];
+       vis[er][ec] = true;
         if(maze[er][ec]=='+') return -1;
         q.add(new Pair(er,ec,0));
         while(!q.isEmpty()){
@@ -15,13 +16,13 @@ class Solution {
              if(isBoundaryAndNotOrignal(p,er,ec,maze)){
                return p.steps;
              }
-             vis[p.row][p.col] = true;
+            
              for(int i = 0 ; i < dirs.length ; i++){
                 int rdash = p.row + dirs[i][0];
                 int cdash = p.col + dirs[i][1];
                 if(rdash<0||cdash<0||rdash==maze.length||cdash==maze[0].length||vis[rdash][cdash]||maze[rdash][cdash]=='+')                 continue;
                 q.add(new Pair(rdash,cdash,p.steps+1));
-                maze[rdash][cdash] = '+';
+                vis[rdash][cdash] = true;
              }
            //  vis[p.row][p.col] = false;
            }
