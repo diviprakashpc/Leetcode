adb5d7b4b658123574c64b1cc3bc17acd7579070
Divya Prakash <diviprakashpc@gmail.com>
2023-02-12 15:08:35 +0530

    Time: 60 ms (19.07%), Space: 51.7 MB (41.18%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,25 +1,23 @@
 class Solution {
-    public int totalFruit(int[] fruits) {
-        // Hash map 'basket' to store the types of fruits.
-        Map<Integer, Integer> basket = new HashMap<>();
-        int left = 0, right;
-        
-        // Add fruit from right side (right) of the window.
-        for (right = 0; right < fruits.length; ++right) {
-            basket.put(fruits[right], basket.getOrDefault(fruits[right], 0) + 1);
-            
-            // If the current window has more than 2 types of fruit,
-            // we remove one fruit from the left index (left) of the window.
-            if (basket.size() > 2) {
-                basket.put(fruits[left], basket.get(fruits[left]) - 1);
-                if (basket.get(fruits[left]) == 0)
-                    basket.remove(fruits[left]);
-                left++;
+    public int totalFruit(int[] nums) {
+        Map<Integer,Integer> map = new HashMap<>();
+        int i = 0;
+        int j = 0;
+        int ans = 0;
+        while(j < nums.length){
+            map.put(nums[j],map.getOrDefault(nums[j],0)+1);
+            if(map.size()<=2){
+                ans = Math.max(ans,j-i+1);
+            }else{
+                 while(map.size()>2){
+                      map.put(nums[i],map.getOrDefault(nums[i],0)-1);
+                     if(map.get(nums[i])==0) map.remove(nums[i]);
+                     i++;
+                 }
+                 ans = Math.max(ans,j-i+1);
             }
+            j++;
         }
-        
-        // Once we finish the iteration, the indexes left and right 
-        // stands for the longest valid subarray we encountered.
-        return right - left;
+        return ans;
     }
 }
