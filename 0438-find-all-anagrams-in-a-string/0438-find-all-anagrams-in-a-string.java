4c2719a0a19d0fbbe1fa4c4e7968801aacf3c401
Divya Prakash <diviprakashpc@gmail.com>
2023-02-12 14:33:34 +0530

    Time: 9 ms (55.33%), Space: 43.6 MB (18.65%) - LeetHub

---
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -0,0 +1,40 @@
+class Solution {
+    List<Integer> ans;
+    public List<Integer> findAnagrams(String s, String p) {
+        ans = new ArrayList<>();
+        checkInclusion(p,s);
+        return ans;
+    }
+    
+     public void checkInclusion(String s1, String s2) {
+        int[] f1  = new int[26];
+        int[] f2 = new int[26];
+        for(int i = 0 ; i < s1.length() ; i++) f1[s1.charAt(i)-'a']++;
+        int m  = s1.length();
+        int n = s2.length();
+        int i  = 0 ;
+        int j = 0;
+        
+        while(j < s2.length()){
+            if(j-i+1<m){
+                f2[s2.charAt(j)-'a']++;
+            }else{
+                 f2[s2.charAt(j)-'a']++;
+                 if(matching(f1,f2)) ans.add(i);
+                 f2[s2.charAt(i)-'a']--;
+                 if( f2[s2.charAt(i)-'a']<=0)  f2[s2.charAt(i)-'a'] = 0;
+                i++;
+            }
+            j++;
+        }
+        
+        return;
+    }
+    
+    public boolean matching(int[] f1 , int[] f2){
+        for(int i = 0 ; i < 26  ; i++){
+            if(f1[i]!=f2[i]) return false;
+        }
+        return true;
+    }
+}
