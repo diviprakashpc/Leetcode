c9922fa65955a616d3fe20d2937e03d6e22bf577
Divya Prakash <diviprakashpc@gmail.com>
2023-08-05 12:39:37 +0530

    Added solution - LeetHub

--- Rearrange Array Alternately - GFG/rearrange-array-alternately.java
+++ Rearrange Array Alternately - GFG/rearrange-array-alternately.java
@@ -67,7 +67,6 @@ class Solution{
         for(int i = 0 ; i < n ; i++){
             if(i%2==0){
                 arr[i] = arr[i] + (arr[k]%mx)*mx;
-                //Here we do %mx because, suppos 
                 k--;
             }else{
                 arr[i] = arr[i]+ (arr[j]%mx)*mx;
@@ -84,5 +83,13 @@ class Solution{
     }
     
 }
+
+//We are doing %mx because,
+// [1,2,3,4,5,6,7,8,9]
+// when i = 1, arr[i] = arr[i] + (arr[j])*mx  if we dont do %mx.
+// thereore arr[1] = arr[1] + (arr[0])*mx;
+// Then arr[1] = 2 + (1+9*mx)*mx . This is because during i = 0  arr[0] got updated.
+//But orignally we wanted arr[1] = 2 + 1*mx , so later we can devide by mx and take out 1 at i= 1
+// So as to extract 1 from this 1+9*mx... we do %mx also.
 
 
